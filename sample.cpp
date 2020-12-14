8186701fc72b18cfffd8e734cac2c611140ee07c
shiba <shiba@shiba.com>
2020-12-14 18:05:05 +0900

    サンプルにUKFを追加

--- sample.cpp
+++ sample.cpp
@@ -19,8 +19,8 @@ int main() {
             0,  0,         1;
         Eigen::Matrix<double, 3, 3> B;
         B << 0.5, 0, 0,
-            0, 0.5, 0,
-            0, 0, 0.5;
+             0, 0.5, 0,
+             0, 0, 0.5;
         Eigen::Matrix<double, 3, 3> Q;
         Q << 1, 0, 0,
             0, 1, 0,
@@ -31,12 +31,12 @@ int main() {
         R << 20;
         Eigen::Matrix<double, 3, 1> initX;
         initX << 5,
-                1,
-                1;
+                 1,
+                 1;
         Eigen::Matrix<double, 3, 3> initKyobunsan;
         initKyobunsan << 1, 0, 0,
-                        0, 1, 0,
-                        0, 0, 1;
+                         0, 1, 0,
+                         0, 0, 1;
         kf.SetStateSpaceModelCoefficientMatrix(A);
         kf.SetSystemMatrix(B);
         kf.SetSystemNoiseMatrix(Q);
@@ -84,28 +84,19 @@ int main() {
 
         std::vector<std::function<double(Eigen::MatrixXd)>> f(1);
         f[0] = [](Eigen::MatrixXd x) -> double { return x(0) + 3*std::cos(x(0) / 10.0); };
-
         ExtendedKalmanFilter::FunctionVector df(f.size() * x.size());
         df[0] = [](Eigen::MatrixXd x) -> double { return 1 - 3.0/10.0 * std::sin(x(0) / 10.0); };
-
         ExtendedKalmanFilter::FunctionVector h(1);
         h[0] = [](Eigen::MatrixXd x) -> double { return x(0); };
-
         ExtendedKalmanFilter::FunctionVector dh(h.size() * x.size());
         dh[0] = [](Eigen::MatrixXd x) -> double { return 1; };
 
-        std::cout << f[0](x) << std::endl;
-
-
         Eigen::Matrix<double, 1, 1> B;
         B << 1.0;
-
         Eigen::Matrix<double, 1, 1> Q;
         Q << 1.0;
-
         Eigen::Matrix<double, 1, 1> R;
         R << 100.0;
-
         Eigen::Matrix<double, 1, 1> P;
         P << 1;
 
@@ -147,20 +138,89 @@ int main() {
             fprintf(gplot,"%d\t%f\n", i, shin[i]);
         }
         fprintf(gplot,"e\n");
-
         for (int i = 0; i < sample_nums; ++i) {
             fprintf(gplot,"%d\t%f\n", i, Y[i]);
         }
         fprintf(gplot,"e\n");
-
         for (int i = 0; i < sample_nums; ++i) {
             fprintf(gplot,"%d\t%f\n", i, esti[i]);
         }
         fprintf(gplot,"e\n");
-
         fflush(gplot);
         pclose(gplot);
     }
 
-    return 0;
+    {
+        UnscentedKalmanFilter ukf;
+
+        Eigen::Matrix<double, 1, 1> x;
+        x << 10;
+
+        std::vector<std::function<double(Eigen::MatrixXd)>> f(1);
+        f[0] = [](Eigen::MatrixXd x) -> double { return x(0) + 3*std::cos(x(0) / 10.0); };
+        ExtendedKalmanFilter::FunctionVector df(f.size() * x.size());
+        df[0] = [](Eigen::MatrixXd x) -> double { return 1 - 3.0/10.0 * std::sin(x(0) / 10.0); };
+        ExtendedKalmanFilter::FunctionVector h(1);
+        h[0] = [](Eigen::MatrixXd x) -> double { return x(0); };
+        ExtendedKalmanFilter::FunctionVector dh(h.size() * x.size());
+        dh[0] = [](Eigen::MatrixXd x) -> double { return 1; };
+
+        Eigen::Matrix<double, 1, 1> B;
+        B << 1.0;
+        Eigen::Matrix<double, 1, 1> Q;
+        Q << 1.0;
+        Eigen::Matrix<double, 1, 1> R;
+        R << 100.0;
+        Eigen::Matrix<double, 1, 1> P;
+        P << 1;
+
+        ukf.Setk(3);
+        ukf.SetStateSpaceModelFunction(f);
+        ukf.SetStateSpaceModelCoefficientJacobian(df);
+        ukf.SetObservationFunction(h);
+        ukf.SetObservationFunctionJacobian(dh);
+        ukf.SetSystemMatrix(B);
+        ukf.SetSystemNoiseMatrix(Q);
+        ukf.SetObservationNoiseMatrix(R);
+        ukf.SetInitialStateMatrix(x);
+        ukf.SetInitialKyobunsanMatrix(P);
+
+        Eigen::Matrix<double, 1, 1> data;
+
+        random_device seed_gen;
+        default_random_engine engine(seed_gen());
+        normal_distribution<> dist(0.0, 1.0);
+
+        int sample_nums = 50;
+        double Y[sample_nums] = {};
+        double shin[sample_nums] = {};
+        double esti[sample_nums] = {};
+        double xk1 = 10;
+        for (int i = 0; i < sample_nums; ++i) {
+            Y[i] = xk1 + dist(engine) * 1.0;
+            shin[i] = xk1;
+            data << Y[i];
+            esti[i] = ukf.Update(data)(0, 0);
+            cout << shin[i] << " " << Y[i] << " " << esti[i] << endl;
+            xk1 = xk1 + 3*cos(xk1 / 10.0);
+        }
+
+        FILE *gplot;
+        gplot = popen("gnuplot -persist","w");
+        fprintf(gplot, "plot '-', '-', '-'\n");
+        for (int i = 0; i < sample_nums; ++i) {
+            fprintf(gplot,"%d\t%f\n", i, shin[i]);
+        }
+        fprintf(gplot,"e\n");
+        for (int i = 0; i < sample_nums; ++i) {
+            fprintf(gplot,"%d\t%f\n", i, Y[i]);
+        }
+        fprintf(gplot,"e\n");
+        for (int i = 0; i < sample_nums; ++i) {
+            fprintf(gplot,"%d\t%f\n", i, esti[i]);
+        }
+        fprintf(gplot,"e\n");
+        fflush(gplot);
+        pclose(gplot);
+    }
 }
